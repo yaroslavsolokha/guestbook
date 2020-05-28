04d71b2646de563ebe8b7616a934ec6499d21d35
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2020-05-28 11:47:58 +0200

    add form and upload

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,7 +7,9 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,9 +18,16 @@ use Twig\Environment;
 class ConferenceController extends AbstractController
 {
   private $twig;
+  private $entityManager;
 
-  public function __construct(Environment $twig) {
+  /**
+   * ConferenceController constructor.
+   * @param Environment $twig
+   * @param EntityManagerInterface $entityManager
+   */
+  public function __construct(Environment $twig, EntityManagerInterface $entityManager) {
     $this->twig = $twig;
+    $this->entityManager = $entityManager;
   }
 
   /**
@@ -37,17 +46,38 @@ class ConferenceController extends AbstractController
 
   /**
    * @Route("/conference/{slug}", name="conference")
+   * @param Request $request
    * @param Conference $conference
    * @param CommentRepository $commentRepository
    * @param ConferenceRepository $conferenceRepository
-   * @return Response
+   * @param string $photoDir
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+   * @throws \Exception
    * @throws \Twig\Error\LoaderError
    * @throws \Twig\Error\RuntimeError
    * @throws \Twig\Error\SyntaxError
    */
-  public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository) {
+  public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository, string $photoDir) {
     $comment = new Comment();
     $form = $this->createForm(CommentFormType::class, $comment);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $comment->setConference($conference);
+      if ($photo = $form['photo']->getData()) {
+        $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+        try {
+          $photo->move($photoDir, $filename);
+        } catch (FileException $e) {
+          // unable to upload the photo, give up
+        }
+
+        $comment->setPhotoFilename($filename);
+      }
+      $this->entityManager->persist($comment);
+      $this->entityManager->flush();
+      return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+    }
 
     $offset = max(0, $request->query->getInt('offset', 0));
     $paginator = $commentRepository->getCommentPaginator($conference, $offset);
