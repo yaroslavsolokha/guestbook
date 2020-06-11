2dbd9ace393207fd2aa871e1846d40223a14ca49
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2020-06-11 10:08:19 +0200

    apply mailer

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Message\CommentMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Workflow\Registry;
+use Twig\Environment;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+  private $twig;
+  private $entityManager;
+  private $bus;
+
+  public function __construct(Environment $twig, EntityManagerInterface $entityManager, MessageBusInterface $bus)
+  {
+    $this->twig = $twig;
+    $this->entityManager = $entityManager;
+    $this->bus=$bus;
+  }
+
+  /**
+   * @Route("/admin/comment/review/{id}", name="review_comment")
+   */
+  public function reviewComment(Request $request, Comment $comment, Registry $registry)
+  {
+    $accepted = !$request->query->get('reject');
+    $machine = $registry->get($comment);
+
+    if ($machine->can($comment,'publish')) {
+      $transition = $accepted ? 'publish' : 'reject';
+    } elseif ($machine->can($comment,'publish_ham')) {
+      $transition = $accepted ? 'publish_ham' : 'reject_ham';
+    } else {
+      return new Response('Comment already reviewed or not in the rightstate.');
+    }
+
+    $machine->apply($comment, $transition);
+    $this->entityManager->flush();
+
+    if ($accepted) {
+      $reviewUrl = $this->generateUrl('review_comment', ['id' =>$comment->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+      $this->bus->dispatch(new CommentMessage($comment->getId(),$reviewUrl));
+    }
+
+    return $this->render('admin/review.html.twig', [
+      'transition' => $transition,
+      'comment'=>$comment,
+    ]);
+  }
+}
